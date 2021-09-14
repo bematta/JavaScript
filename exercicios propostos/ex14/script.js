64e6ffcf292abe9cebd78e408e9d94eba9b6626a
Beatriz Matta <bmcluiz@gmail.com>
2021-09-13 23:02:01 -0300

    versão estável, ex 15
    exercício 15 com a base funcionando

--- exercicios propostos/ex14/script.js
+++ exercicios propostos/ex14/script.js
@@ -9,14 +9,20 @@ function clicar () {
 	var data = new Date()
 	var hora = data.getHours()
 	var minuto = data.getMinutes()
-	msg.innerHTML= `São ${hora}:${minuto} horas`
+	msg.innerHTML= `São ${hora}:${minuto} horas.`
 
 	if (hora<12) {
 	img.src ='manha.jpg'
+	msg.innerHTML+=` Bom dia!`
+	window.document.body.style.background = '#edcd97'
 	} else if (hora < 18) {
 	img.src ='tarde.jpg'
+	msg.innerHTML+=` Boa tarde!`
+	window.document.body.style.background = '#b9846f'
 	} else {
 	img.src ='noite.jpg'
+	msg.innerHTML+=` Boa noite!`
+	window.document.body.style.background = '#515154'
 	}
 }
 
--- exercicios propostos/ex15/script.js
+++ exercicios propostos/ex15/script.js
@@ -1,1 +1,23 @@
-alert('Olá, visitante!')
+
+
+function enviar() {
+	var data = new Date()
+	var ano_atual = data.getFullYear()
+	var ano_nasc =window.document.getElementById('ano')
+	var res =window.document.getElementById('res')
+
+	if (ano_nasc.value.length==0 || ano_nasc.value>ano_atual) {
+		window.alert(`verifique os dados e tente novamente`)
+	} else {
+		var radsex =window.document.getElementsByName('radsex')
+		var idade = ano_atual-Number(ano_nasc.value)
+		var genero=''
+		if (radsex[0].checked) {
+			genero= 'homem' 
+		} else {
+			genero = 'mulher'
+		}
+
+		res.innerHTML = `Detectamos ${genero} de ${idade} anos`
+	}
+}
